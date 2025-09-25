4d161d1c81cc234d2961fc99628f376e5e891143
shahbazahmad1 <shahbaz.ahmad141@gmail.com>
2025-09-25 17:59:14 +0530

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("Hello world")
+});
+
+app.listen(ENV.PORT, () => {
+    console.log("Server started on port:",ENV.PORT);
+});
